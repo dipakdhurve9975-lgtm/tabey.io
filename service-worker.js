b750723db6496496acdbeeed6dc8566b4a3f8d8b
dipakdhurve9975-lgtm <dipakdhurve9975@gmail.com>
2026-02-12 14:44:47 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,27 @@
-self.addEventListener('install', function(event) {
-  console.log("Service Worker Installed");
+const CACHE_NAME = "tabey-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/offline.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request)
+        .then(response => {
+          return response || caches.match("/offline.html");
+        });
+    })
+  );
 });
